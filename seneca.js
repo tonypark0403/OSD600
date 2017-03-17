4b7a6d3ef18bd32163e5545a38981fec3f213ada
Tony Park <tonypark0403@gmail.com>
2017-03-17 15:21:13 -0400

    modified seneca.js to fix all errors during eslint

--- seneca.js
+++ seneca.js
@@ -5,19 +5,19 @@
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
-exports.isValidEmail = function(email) {
-    // TODO: needs to be implemented
-    if (email.includes('@myseneca.ca')) {
-        return true;
-    }
-    return false;
+exports.isValidEmail = function isValidEmail(email) {
+  // TODO: needs to be implemented
+  if (email.includes('@myseneca.ca')) {
+    return true;
+  }
+  return false;
 };
 
 /**
  * Given a string `name`, return a formatted Seneca email address for
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
-exports.formatSenecaEmail = function(name) {
-    // TODO: needs to be implemented
-    return name + '@myseneca.ca';
+exports.formatSenecaEmail = function formatSenecaEmail(name) {
+  // TODO: needs to be implemented
+  return name.concat('@myseneca.ca');
 };
